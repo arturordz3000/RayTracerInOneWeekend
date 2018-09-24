279cad2b0bc734880dd26a8ea9179bf0e86459da
Arturo de Jesús Rodriguez Arteaga <arturo.rdz3000@gmail.com>
2018-09-23 20:15:32 -0500

    Added sphere normals

--- RaytracerInOneWeekend/RaytracerInOneWeekend.cpp
+++ RaytracerInOneWeekend/RaytracerInOneWeekend.cpp
@@ -12,7 +12,7 @@ void setFile()
     std::cout.rdbuf(out.rdbuf());
 }
 
-bool hit_sphere(const vec3& center, float radius, const ray& r)
+float hit_sphere(const vec3& center, float radius, const ray& r)
 {
     vec3 oc = r.origin() - center;
     float a = dot(r.direction(), r.direction());
@@ -20,19 +20,43 @@ bool hit_sphere(const vec3& center, float radius, const ray& r)
     float c = dot(oc, oc) - radius * radius;
     float discriminant = b * b - 4 * a * c;
     
-    return discriminant > 0;
+    // A discriminant lesser than 0 means that there's no hit point
+    if (discriminant < 0)
+    {
+        return -1.0;
+    }
+    else
+    {
+        // This is the general formula to solve a quadratic equation
+        return (-b - sqrt(discriminant)) / (2 * a);
+    }
 }
 
 vec3 color(const ray& r)
 {
-    if (hit_sphere(vec3(0, 0, -1), 0.5, r))
-        return vec3(1, 0, 0);
+    // Sphere center
+    vec3 c = vec3(0, 0, -1);
+    
+    // t is the result of the quadratic equation Ax2 + 2Bx + C = 0
+    float t = hit_sphere(c, 0.5, r);
+    
+    if (t >= 0)
+    {
+        // Hit point
+        vec3 p = r.point_at_parameter(t);
+        
+        // The normal vector of a sphere is given by the hit point minus the sphere center (p - c)
+        vec3 N = unit_vector(p - c);
+        
+        // Converting the normal vector to the range of 0 to 1
+        return 0.5 * vec3(N.x() + 1, N.y() + 1, N.z() + 1);
+    }
     
     vec3 unit_direction = unit_vector(r.direction());
     
     // Since the unit_direction variable can have positive and negative values in its "Y" coordinate (-1 < Y < 1),
     // we need to tranform those values so that 0 < t < 1;
-    float t = 0.5 * (unit_direction.y() + 1);
+    t = 0.5 * (unit_direction.y() + 1);
     
     // We now interpolate between white (1, 1, 1) and blue (0.5, 0.7, 1.0) depending on the ray's up/downess
     return (1 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
